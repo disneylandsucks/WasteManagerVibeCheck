d6cf4125f79564125c4dadf6b7d57cc986f3d723
painkillerkid <91149449+painkillerkid@users.noreply.github.com>
2022-12-01 18:39:12 -0800

    Update Shirt.js

--- Shirt.js
+++ Shirt.js
@@ -7,6 +7,8 @@ class Shirt{
     this.theta = theta
     this.rotationVal = 180
     this.dragging = false
+    this.rotateShirt = false
+    this.autoRotateValue = 0
   }
 
   changeShirt(img){
@@ -15,7 +17,9 @@ class Shirt{
 
   drag(mX, mY){
     this.startDrag = createVector(mX, mY, 0)
+    this.lastMousePos = this.startDrag
     this.dragging = true
+
   }
 
   render(){
@@ -25,7 +29,7 @@ class Shirt{
       scale(50.0)
       translate(3, 8, 3)
       rotateX(180)
-      rotateY(this.rotationVal)
+      rotateY(this.rotationVal + this.autoRotateValue)
       // rotateZ(this.theta/4)
 
       scale(-1, 1)
@@ -35,17 +39,59 @@ class Shirt{
     pop()
   }
 
-update(mX, mY){
+  getDelta(currentMousePos){
+   //console.log(currentMousePos, this.lastMousePos)
+
+     return currentMousePos.dist(this.lastMousePos)
+   }
+
+   toggleRotate(){
+    this.rotateShirt = !this.rotateShirt
+    
+   }
+
+
+  update(mX, mY, theta){
     if(this.dragging){
+    console.log('being dragged')      
+      // this.startDrag was set when mouse was clicked
+      // we need to know if mouse is still moving
+     
       const currentMousePos = createVector(mX, mY, 0)
+      
+      const delta = this.getDelta(currentMousePos)
+      console.log(delta)
       //console.log(currentMousePos)
-      const d = currentMousePos.dist(this.startDrag)
-      if(currentMousePos.y > this.startDrag.y){
-        this.rotationVal = d
-      }else{
-        this.rotationVal += d * -1
+      if(delta > 0 || delta < 0){
+        let d = currentMousePos.dist(this.startDrag)
+        if(d > 10){
+          d = 10
+        }
+        if(d < -10){
+          d = -10
+        }
+        // this.rotationVal -= d
+
+        if(currentMousePos.y > this.startDrag.y){
+          this.rotationVal -= d
+        }else{
+          this.rotationVal += d
+        }
       }
 
+      this.lastMousePos = currentMousePos
+    } else {
+      
+      if(this.rotateShirt){
+        console.log(theta)
+        console.log('should rotate')
+        
+          this.autoRotateValue += theta
+       
+        
+      }
     }
 
+    
   }
+}
