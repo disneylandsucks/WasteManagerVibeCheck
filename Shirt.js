57fee45703139e8d2c7119c9619af0dddf3e8e88
painkillerkid <91149449+painkillerkid@users.noreply.github.com>
2022-12-01 18:44:03 -0800

    Update Shirt.js

--- Shirt.js
+++ Shirt.js
@@ -1,97 +1,196 @@
-class Shirt{
-  constructor(img, mod, x, y, z, theta){
-    this.img = img
-    this.mod = mod
-    this.pos = createVector(x,y,z)
-    this.startDrag = null
-    this.theta = theta
-    this.rotationVal = 180
-    this.dragging = false
-    this.rotateShirt = false
-    this.autoRotateValue = 0
-  }
 
-  changeShirt(img){
-    this.img = img
-  }
+const canvasWidth = window.innerWidth
+const canvasHeight = window.innerHeight
+let img, img2, mod, shirt
+let wg, bg, fg, money,venmo
+let imgs = [], imageCounter = 0
+let theta = 0
+let thetaInc = 0.01
+let rotating = false
+let clicked = false
 
-  drag(mX, mY){
-    this.startDrag = createVector(mX, mY, 0)
-    this.lastMousePos = this.startDrag
-    this.dragging = true
+function preload(){
+    // audio = createAudio("Katy Perry - Firework (Acoustic Session) REMASTERED.mp3");
+    audio = createAudio("Carly Rae Jepsen - Call Me Maybe (Audio) [TubeRipper.com].m4a");
 
-  }
+imgs[0]= loadImage('https://res.cloudinary.com/painkillerkid/image/upload/v1665773929/SCREENPRINT_Shirt_resized_ujjqp1.png')
+ imgs[1] = loadImage('https://res.cloudinary.com/painkillerkid/image/upload/v1665773929/voldemort_shirt_resized_ytqhl7.png') 
+    imgs[2]= loadImage('https://res.cloudinary.com/painkillerkid/image/upload/v1665773929/OSKI_BIRTHDAY_SHIRT3d_resized_or4zka.png')
+ imgs[3] = loadImage('https://res.cloudinary.com/painkillerkid/image/upload/v1665773929/DemnaShirt_resized_ko0nu6.png') 
+  imgs[4]= loadImage('https://res.cloudinary.com/painkillerkid/image/upload/v1665773930/carolshirt3d_resized_xs8lip.png')
+  imgs[5]= loadImage('https://res.cloudinary.com/painkillerkid/image/upload/v1665773929/bottledloveshirt3d_flsb0r.png')
+  imgs[6] = loadImage('https://res.cloudinary.com/painkillerkid/image/upload/v1665884116/NOTEBOOKSHIRTTEXT_w3jjsn.jpg')
+    imgs[7] = loadImage('https://res.cloudinary.com/painkillerkid/image/upload/v1665884680/yourCardDeclined_ShirtMockup_vnw9pc.png')    
+  imgs[8] = loadImage('  https://res.cloudinary.com/painkillerkid/image/upload/v1665888592/OSKIWEDDING_SHIRT_tz6okn.png')
+    imgs[9] = loadImage(' https://res.cloudinary.com/painkillerkid/image/upload/v1665948981/oskiweddinground2_egydil.png')
+      imgs[10] = loadImage(' https://res.cloudinary.com/painkillerkid/image/upload/v1665949784/toadpacshirt_3dthing_j0satg.png')
 
-  render(){
-    noStroke()
-    push()
-      translate(this.pos.x, this.pos.y, this.pos.z)
-      scale(50.0)
-      translate(3, 8, 3)
-      rotateX(180)
-      rotateY(this.rotationVal + this.autoRotateValue)
-      // rotateZ(this.theta/4)
-
-      scale(-1, 1)
-      texture(this.img)
-
-      model(this.mod)
-    pop()
-  }
-
-  getDelta(currentMousePos){
-   //console.log(currentMousePos, this.lastMousePos)
-
-     return currentMousePos.dist(this.lastMousePos)
-   }
-
-   toggleRotate(){
-    this.rotateShirt = !this.rotateShirt
+        imgs[11] = loadImage(' https://res.cloudinary.com/painkillerkid/image/upload/v1665957356/housedrawing_3dShirt_nf3yob.png')
+  
+        imgs[12] = loadImage(' https://res.cloudinary.com/painkillerkid/image/upload/v1665957205/mountainshirt_3d_kmlazm.png')
+            
+  imgs[13] = loadImage(' https://res.cloudinary.com/painkillerkid/image/upload/v1665978926/oski_peeing_shirt_xtvoe3.png')
     
-   }
+  imgs[14] = loadImage(' https://res.cloudinary.com/painkillerkid/image/upload/v1666047243/thisisstupidshirt_wltht1.png')  
+    imgs[20] = loadImage('   https://res.cloudinary.com/painkillerkid/image/upload/v1669923666/musuem_shirt_ktuln1.png')
+  
+  imgs[15] = loadImage(' https://res.cloudinary.com/painkillerkid/image/upload/v1669922268/Finished_House_Shirt_vx2eox.png')
+  
+    imgs[16] = loadImage(' https://res.cloudinary.com/painkillerkid/image/upload/v1669922268/fINISHED_HOUSE_sHIRT2_ncbeuk.png')
+  
+      imgs[17] = loadImage(' https://res.cloudinary.com/painkillerkid/image/upload/v1669922500/MiddleFinger_Finished_Shirt_mdd5jo.png')
+         imgs[18] = loadImage('  https://res.cloudinary.com/painkillerkid/image/upload/v1665961018/MXMTOON_Shirt2_jh50p3.png')
 
-
-  update(mX, mY, theta){
-    if(this.dragging){
-    console.log('being dragged')      
-      // this.startDrag was set when mouse was clicked
-      // we need to know if mouse is still moving
-     
-      const currentMousePos = createVector(mX, mY, 0)
-      
-      const delta = this.getDelta(currentMousePos)
-      console.log(delta)
-      //console.log(currentMousePos)
-      if(delta > 0 || delta < 0){
-        let d = currentMousePos.dist(this.startDrag)
-        if(d > 10){
-          d = 10
-        }
-        if(d < -10){
-          d = -10
-        }
-        // this.rotationVal -= d
-
-        if(currentMousePos.y > this.startDrag.y){
-          this.rotationVal -= d
-        }else{
-          this.rotationVal += d
-        }
-      }
-
-      this.lastMousePos = currentMousePos
-    } else {
-      
-      if(this.rotateShirt){
-        console.log(theta)
-        console.log('should rotate')
-        
-          this.autoRotateValue += theta
-       
-        
-      }
-    }
-
-    
-  }
+  
+  imgs[19] = loadImage('  https://res.cloudinary.com/painkillerkid/image/upload/v1669922804/ChaeyoungFromTwice_ImTryingtoDate_vftqn8.png')
+  
+  
+  imgs[21] = loadImage(' https://www.pinkvilla.com/files/styles/amp_metadata_content_image/public/kevin-hart-wins-halloween-2019-by-dressing-up-as-frenemy-dwayne-johnson-the-rock-is-anything-but-impressed.jpg')
+  
+  
+  mod = loadModel('Finished_Voldemort_3dthing.obj')
 }
+
+
+function setup() {
+  createCanvas(canvasWidth, canvasHeight, WEBGL);
+  angleMode(DEGREES)
+  shirt = new Shirt(imgs[0], mod, -100, -100, 0, theta)
+    wg = loadImage ('https://res.cloudinary.com/painkillerkid/image/upload/v1669760216/wordstuff2_iqvi3a.png');
+  
+  fg = loadImage ('https://archive.org/download/spongedance-hd/spongedance-4.gif')
+  
+  bg = loadImage ('https://media.tenor.com/2roX3uxz_68AAAAM/cat-space.gif')
+ 
+    money = loadImage ('https://res.cloudinary.com/painkillerkid/image/upload/v1669858524/sendmemoney_schfml.png')
+  
+      venmo = loadImage ('https://res.cloudinary.com/painkillerkid/image/upload/v1669857644/MYVENMO_bkfuco.jpg')
+}
+
+function draw() {
+  background('rgb(211,246,0)'); 
+  
+    push();
+  noStroke();
+translate(30, 20, -3000);
+  texture(bg);
+  plane(20000, 20000);
+  pop();  
+  
+  push();
+  noStroke()
+  button = createButton('');
+  if(rotating){
+    button.addClass('unrotate-button')
+    button.html('stop rotation')
+  }else{
+    button.addClass('rotate-button');
+    button.html('rotate shirt')    
+  }
+ 
+  button.position(1300, 650);
+  button.mousePressed(setRotate);
+  pop();
+  
+    push();
+  noStroke();
+translate(1300, -150, -1000);
+  texture(money);
+  plane(500, 500);
+  pop(); 
+  
+      push();
+  noStroke();
+translate(1300, -650, -1000);
+  texture(venmo);
+  plane(500, 500);
+  pop(); 
+  
+  push();
+  noStroke();
+translate(-1130, -200, -1000);
+  texture(fg);
+  plane(1000, 1000);
+  pop(); 
+  
+    push();
+  noStroke();
+translate(-1130, -200, -1000);
+  texture(wg);
+  plane(1000, 1000);
+  pop(); 
+  //audio.autoplay(true);
+  //audio.loop(true);
+  //console.log(theta)
+  shirt.update(mouseX, mouseY, theta)
+  
+ 
+  shirt.render()
+  if(theta > 2 || theta < -2){
+    thetaInc*=-1
+  }
+  theta+=thetaInc
+
+}
+
+function setRotate(){
+  rotating = !rotating
+  shirt.toggleRotate()
+}
+
+function changeBG() {
+  let val = random(255);
+  background(val);
+}
+function mousePressed(){
+
+   shirt.drag(mouseX, mouseY)
+   clicked = true
+
+}
+
+function mouseReleased(){
+
+   shirt.dragging = false
+   clicked = false
+
+}
+
+
+
+
+  // old
+
+// function mousePressed(){ 
+  // if mouse is in right box
+    // then: do the image increment thing
+  //else if mouse is in left box
+    // do the image decrement thing
+  // else
+
+  // imageCounter = floor(random(imgs.length))
+
+//     shirt.changeShirt(imgs[imageCounter])
+
+//}
+
+function keyPressed(){
+  if(keyCode === RIGHT_ARROW){
+    if(imageCounter < imgs.length -1){
+      imageCounter++
+    }else{
+      imageCounter = 0
+    }
+  }
+    if(keyCode === LEFT_ARROW){
+      if(imageCounter > 0){
+         imageCounter--
+      } else {
+        imageCounter = imgs.length - 1
+      }
+
+  }
+ // console.log(imageCounter)
+  shirt.changeShirt(imgs[imageCounter])
+}
+
